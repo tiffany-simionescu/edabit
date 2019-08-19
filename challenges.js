d753959e194650974c7d7adb0e2e2e945ac46421
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-08-19 16:05:42 -0400

    Added the functions lessThanOrEqualToZero, animals and profitableGamble.

--- challenges.js
+++ challenges.js
@@ -75,3 +75,50 @@ function remainder(num1, num2) {
 console.log(remainder(1, 3)); // 1
 console.log(remainder(-9, 45)); // -9
 console.log(remainder(5, 5)); // 0
+
+/*
+    Is the Number Less than or Equal to Zero?
+        - Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+*/
+
+function lessThanOrEqualToZero(num) {
+  if (num <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(lessThanOrEqualToZero(5)); // false
+console.log(lessThanOrEqualToZero(0)); // true
+console.log(lessThanOrEqualToZero(-2)); // true
+
+/*
+    The Farm Problem
+        - You've got chickens (2 legs), cows (4 legs) and pigs (4 legs) on your farm. Return the total number of legs on your farm. 
+*/
+
+function animals(chickens, cows, pigs) {
+  return chickens * 2 + cows * 4 + pigs * 4;
+}
+
+console.log(animals(2, 3, 5)); // 36
+console.log(animals(1, 2, 3)); // 22
+console.log(animals(5, 2, 8)); // 50
+
+/*
+    Profitable Gamble
+        - Create a function that takes in three arguments (prob, prize, pay) and return true if prob * prize > pay; otherwise return false
+*/
+
+function profitableGamble(prob, prize, pay) {
+  if (prob * prize > pay) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(profitableGamble(0.2, 50, 9)); // true
+console.log(profitableGamble(0.9, 1, 2)); // false
+console.log(profitableGamble(0.9, 3, 2)); // true
