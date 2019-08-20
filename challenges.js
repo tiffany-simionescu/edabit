ec04015bb212f720f55481e5b74b8d7dd93f241b
Tiffany Simionescu <tiffanyfeldkamp@gmail.com>
2019-08-20 17:22:37 -0400

    Added the functions isEmpty, concatName and comp. Also added Arrow Function alternatives.

--- challenges.js
+++ challenges.js
@@ -7,6 +7,12 @@ function addition(a, b) {
   return a + b;
 }
 
+/* 
+    Arrow Function
+
+    const addition = (a, b) => a + b;
+*/
+
 console.log(addition(3, 2)); // 5
 console.log(addition(-3, -6)); // -9
 console.log(addition(7, 3)); // 10
@@ -19,6 +25,12 @@ console.log(addition(7, 3)); // 10
 function getFirstValue(arr) {
   return arr[0];
 }
+
+/* 
+    Arrow Function
+
+    const getFirstValue = arr => arr[0];
+*/
 
 console.log(getFirstValue([1, 2, 3])); // 1
 console.log(getFirstValue([80, 5, 100])); // 80
@@ -33,6 +45,12 @@ function triArea(base, height) {
   return (base * height) / 2;
 }
 
+/* 
+    Arrow Function
+
+    const triArea = (base, height) => (base * height) / 2;
+*/
+
 console.log(triArea(3, 2)); // 3
 console.log(triArea(7, 4)); // 14
 console.log(triArea(10, 10)); // 50
@@ -45,6 +63,12 @@ console.log(triArea(10, 10)); // 50
 function nextEdge(side1, side2) {
   return side1 + side2 - 1;
 }
+
+/* 
+    Arrow Function
+
+    const nextEdge = (side1, side2) => side1 + side2 - 1;
+*/
 
 console.log(nextEdge(8, 10)); // 17
 console.log(nextEdge(5, 7)); // 11
@@ -59,6 +83,12 @@ function incrementByOne(num) {
   return num + 1;
 }
 
+/* 
+    Arrow Function
+
+    const incrementByOne = num => num + 1;
+*/
+
 console.log(incrementByOne(0)); // 1
 console.log(incrementByOne(9)); // 10
 console.log(incrementByOne(-3)); // -2
@@ -71,6 +101,12 @@ console.log(incrementByOne(-3)); // -2
 function remainder(num1, num2) {
   return num1 % num2;
 }
+
+/* 
+    Arrow Function
+
+    const remainder = (num1, num2) => num1 % num2;
+*/
 
 console.log(remainder(1, 3)); // 1
 console.log(remainder(-9, 45)); // -9
@@ -89,6 +125,18 @@ function lessThanOrEqualToZero(num) {
   }
 }
 
+/* 
+    Arrow Function
+
+    const lessThanOrEqualToZero = num => {
+        if (num <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+*/
+
 console.log(lessThanOrEqualToZero(5)); // false
 console.log(lessThanOrEqualToZero(0)); // true
 console.log(lessThanOrEqualToZero(-2)); // true
@@ -101,6 +149,12 @@ console.log(lessThanOrEqualToZero(-2)); // true
 function animals(chickens, cows, pigs) {
   return chickens * 2 + cows * 4 + pigs * 4;
 }
+
+/*
+    Arrow Function
+
+    const animals = (chickens, cows, pigs) => chickens * 2 + cows * 4 + pigs * 4;
+*/
 
 console.log(animals(2, 3, 5)); // 36
 console.log(animals(1, 2, 3)); // 22
@@ -119,6 +173,18 @@ function profitableGamble(prob, prize, pay) {
   }
 }
 
+/*
+    Arrow Function
+
+    const profitableGamble = (prob, prize, pay) => {
+       if (prob * prize > pay) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+*/
+
 console.log(profitableGamble(0.2, 50, 9)); // true
 console.log(profitableGamble(0.9, 1, 2)); // false
 console.log(profitableGamble(0.9, 3, 2)); // true
@@ -131,6 +197,12 @@ console.log(profitableGamble(0.9, 3, 2)); // true
 function length(str) {
   return str.length;
 }
+
+/*
+    Arrow Function
+
+    const length = str => str.length;
+*/
 
 console.log(length("apple")); // 5
 console.log(length("make")); // 4
@@ -150,6 +222,18 @@ function divisibleByFive(num) {
   }
 }
 
+/*
+    Arrow Function
+
+    const divisibleByFive = num => {
+        if (num % 5 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+*/
+
 console.log(divisibleByFive(5)); // true
 console.log(divisibleByFive(-55)); // true
 console.log(divisibleByFive(37)); // false
@@ -167,6 +251,95 @@ function divisible(num) {
   }
 }
 
+/*
+    Arrow Function
+
+    const divisible = num => {
+        if (num % 100 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+*/
+
 console.log(divisible(1)); // false
 console.log(divisible(1000)); // true
 console.log(divisible(100)); // true
+
+/*
+    Is the String Empty?
+        - Create a function that returns true if a string is empty and false otherwise.
+*/
+
+function isEmpty(s) {
+  if (s === "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+    Arrow Function
+
+    const isEmpty = s => {
+        if (s === "") {
+            return true;
+        } else {
+            return false;
+        }
+    };
+*/
+
+console.log(isEmpty("")); // true
+console.log(isEmpty(" ")); // false
+console.log(isEmpty("a")); // false
+
+/*
+    Concatenate First and Last Name into One String
+        - Given two strings, firstName and lastName, return a single string in the format "last, first".
+*/
+
+function concatName(firstName, lastName) {
+  return `${lastName}, ${firstName}`;
+}
+
+/* 
+    Arrow Function
+
+    const concatName = (firstName, lastName) => `${lastName}, ${firstName}`;
+*/
+
+console.log(concatName("First", "Last")); // "Last, First"
+console.log(concatName("John", "Doe")); // "Doe, John"
+console.log(concatName("Mary", "Jane")); // "Jane, Mary"
+
+/*
+    Compare Strings by Count of Characters
+        - Create a function that takes two strings as arguments and return either true or false depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+*/
+
+function comp(str1, str2) {
+  if (str1.length === str2.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/* 
+    Arrow Function
+
+    const comp = (str1, str2) => {
+        if (str1.length === str2.length) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+*/
+
+console.log(comp("AB", "CD")); // true
+console.log(comp("ABC", "DE")); // false
+console.log(comp("hello", "edabit")); // false
